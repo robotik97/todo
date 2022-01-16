c96e13de77e686087cfe4fd9f7d46b0fcd8743ad
robotik97 <edhar97@gmail.com>
2022-01-16 22:53:29 +0300

    init: start build app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ToDoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build App</li>
+    </ul>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const AppHeader = () => {
+  return <h1>My ToDo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <ToDoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App/>, document.getElementById("root"));
