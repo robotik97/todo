f6832f03f7cced5fcd0dff9974c46ba732e6b0c0
robotik97 <edhar97@gmail.com>
2022-01-30 14:03:27 +0300

    feat: add components

---
+++ src/components/ToDoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ToDoList = () => {
+  const items = ["learn react", "build app"];
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build App</li>
+      <li>Make awesome app</li>
+    </ul>
+  );
+};
+export default ToDoList;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SearchPanel from "./components/search-panel";
+import AppHeader from "./components/app-header";
+
 const ToDoList = () => {
+  const items = ["learn react", "build app"];
   return (
     <ul>
       <li>Learn React</li>
       <li>Build App</li>
+      <li>Make awesome app</li>
     </ul>
   );
-};
-
-const AppHeader = () => {
-  return <h1>My ToDo List</h1>;
-};
-
-const SearchPanel = () => {
-  return <input placeholder="search" />;
 };
 
 const App = () => {
@@ -28,4 +25,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
