37a05b5ccec39977b45c96c39c7be6d00e5b842e
robotik97 <edhar97@gmail.com>
2022-02-10 23:07:34 +0300

    refactor: change structure of the project

--- src/components/to-do-list-item/to-do-list-Item.js
+++ src/components/to-do-list-item/to-do-list-Item.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./to-do-list-item.css"
 
 const ToDoListItem = ({ label, important = false }) => {
   // change color item if it important
   const changeColor = {
-    color: important ? "tomato" : "black",
+    color: important ? "violet" : "black",
   };
   return <span style={changeColor}> {label}</span>;
+  <button type="button"></button>
 };
 export default ToDoListItem;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 
 import SearchPanel from "./components/search-panel";
 import AppHeader from "./components/app-header";
-import ToDoList from "./components/ToDoList";
+import ToDoList from "./components/to-do-list";
 
 const App = () => {
   const toDoData = [
-    { label: 'Eat burger', important: false },
-    { label: 'Build awesome app', important: true },
-    { label: 'Sleep', important: false },
+    { label: "Eat burger", important: false, id: "1" },
+    { label: "Build awesome app", important: true, id: "2" },
+    { label: "Sleep", important: false, id: "3" },
   ];
   return (
     <div>
