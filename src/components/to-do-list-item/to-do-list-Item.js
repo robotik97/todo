33f67a3dce3f8f0e14ff41cfbeba52f4bb273ff9
robotik97 <edhar97@gmail.com>
2022-03-19 22:43:53 +0300

    refactor:add events to buttons and items

--- src/components/to-do-list-item/to-do-list-Item.js
+++ src/components/to-do-list-item/to-do-list-Item.js
@@ -1,34 +1,64 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./to-do-list-item.css";
 // create a class to get the internal state of component
 export default class ToDoListItem extends Component {
-  render (){
-// destructuring properties from object props. It's the same
-// props.label = {label}
-// props.important = {important}
-// change color and font weight item if it important
-    const { label, important = false } = this.props
-    const changeStyle = {
-      color: important ? "violet" : "black",
-      fontWeight: important? "bold" : "normal"
-    };
+  state = {
+    done: false,
+    important:false
+  }
+  onLabelClick = () => {
+    this.setState((state) => {
+      return {
+        done: !state.done
+      }
+    })
+  };
+
+  onMarkImportant = () => {
+    this.setState( (state) =>{
+      return {
+        important: !state.important
+      }
+    })
+  } 
+  
+  render() {
+    // destructuring properties from object props. It's the same
+    // props.label = {label}
+    // props.important = {important}
+    // change color and font weight item if it important
+    const { label} = this.props;
+    const {done, important} = this.state;
+  
+    let classNames = "todo-list-item";
+    // done & important take from the state
+    if (done) {
+      classNames += " done";
+    }
+    if (important) {
+      classNames += " important"
+    }
+
     return (
-      <span className="todo-list-item" >
-        <span style={changeStyle}> {label} </span>
-        
-    <span >
-        <button
-          type="button"
-          className="btn btn-outline-success btn-sm float-right ">
-          <i className="fa-solid fa-thumbtack"></i>
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa-solid fa-trash-can"></i>
-        </button>
+      <span className= {classNames}>
+        <span onClick={this.onLabelClick}>
+          {label}
         </span>
+
+        <span>
+          <button
+            type="button"
+            className="btn btn-outline-success btn-sm float-right "
+            onClick={this.onMarkImportant}>
+            <i className="fa-solid fa-thumbtack"></i>
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm float-right">
+            <i className="fa-solid fa-trash-can"></i>
+          </button>
         </span>
+      </span>
     );
   }
 }
